9f5ed8442c321360b2c6522739c81837c57d14d0
tejasmahajan2 <tejas.mahajan@in.spundan.com>
2025-02-15 10:54:11 +0530

    switched to redis tcp

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.REDIS,
       options: {
         host: process.env.MATH_SERVICE_HOST,
-        port: +process.env.MATH_SERVICE_HOST || 3001, // Microservice listens on this port
+        port: +process.env.MATH_SERVICE_HOST || 54177, // Microservice listens on this port
       },
     }
   );
